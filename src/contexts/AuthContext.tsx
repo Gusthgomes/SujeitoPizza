5a1c2bff1086c037dea0c14c5089c44771d9296b
Gustavo Gomes <heiqueenn@gmail.com>
2023-08-02 20:23:24 -0300

    fix: keeping the user in the applicaion

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
 
 import { api } from '../services/apiClient';
 
@@ -52,6 +52,30 @@ export function signOut(){
 export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    
+    // tentar pegar algo no cookie
+    const { '@nextauth.token': token } = parseCookies();
+
+    if(token){
+      api.get('/me').then(response => {
+        const { id, name, email } = response.data;
+
+        setUser({
+          id,
+          name,
+          email
+        })
+      })
+
+      // em caso de erro, deslogamos o usuário
+      .catch(() => {
+        signOut();
+      })
+    }
+    
+  }, [])
 
   async function signIn({ email, password }: SignInProps){
     try{
