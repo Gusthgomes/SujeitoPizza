98f30f9fa04d72a3ca9bcf66dcd858cb782644b1
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2023-08-10 20:46:13 -0300

    feat: fetching the backend data

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,32 @@
-
+import { useState } from 'react';
 import { canSSRAuth } from '../../utils/canSSRAuth';
 import Head from 'next/head';
 import styles from './styles.module.scss';
 import { FiRefreshCcw } from 'react-icons/fi';
 
 import { Header } from '../../components/Header';
+import { setupAPIClient } from '../../services/api';
 
-export default function Dashboard(){
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface HomeProps{
+  orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps ){
+
+  const [orderList, setOrderList] = useState(orders || []);
+
+  function handleOpenModalView(id: string){
+    alert("Id: " + id)
+  }
+
   return(
    <>
    <Head>
@@ -25,16 +45,16 @@ export default function Dashboard(){
       </div>
 
       <article className={styles.listOrders}>
-        <section className={styles.orderItem}>
 
-          <button>
+        {orderList.map( item => (
+          <section key={item.id} className={styles.orderItem}>
+            <button onClick={ () => handleOpenModalView(item.id) }>
+              <div className={styles.tag}></div>
+              <span>Mesa {item.table} </span>
+            </button>
+          </section>
+        ))}
 
-            <div className={styles.tag}></div>
-            <span>Mesa 22</span>
-
-          </button>
-
-        </section>
       </article>
 
     </main>
@@ -45,7 +65,15 @@ export default function Dashboard(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get('/orders');
+
+  console.log(response.data)
+
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
